5e105e3da06a39ce8dc187991ff3616cb835a785
SatyamIndhane <satyam.indhane@gmail.com>
2021-04-07 11:16:36 +0530

    Address Book App

---
+++ Scripts/Contact Form.js
@@ -0,0 +1,132 @@
+let isUpdate=false;
+let newContact=new Array();
+
+window.addEventListener('DOMContentLoaded',(event)=>{
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input',function () {
+        if (name.value.length == 0) {
+            textError.textContent = "*Name field is empty!!";
+            return;
+        }
+        try {
+            (new Contacts()).name = name.value;
+            textError.textContent = "";
+        } catch (exception) {
+            textError.textContent = exception;
+        }
+    });
+
+    const phone = document.querySelector('#phone');
+    const phoneError = document.querySelector('.phone-error');
+    phone.addEventListener('input',function () {
+        if (phone.value.length == 0) {
+            phoneError.textContent = "*No Number Found!!";
+            return;
+        }
+        try {
+            (new Contacts()).phone = phone.value;
+            phoneError.textContent = "";
+        } catch (exception) {
+            phoneError.textContent = exception;
+        }
+    });
+
+    const address = document.querySelector('#address');
+    const addressError = document.querySelector('.address-error');
+    address.addEventListener('input',function () {
+        if (address.value.length == 0) {
+            addressError.textContent = "*No Entry Found!!";
+            return;
+        }
+        try {
+            (new Contacts()).address = address.value;
+            addressError.textContent = "";
+        } catch (exception) {
+            addressError.textContent = exception;
+        }
+    });
+    checkForUpdates();
+});
+
+function save(){
+    try{
+        //contact._id=getID();
+        contact._name=document.getElementById('name').value;
+        contact._phone=document.getElementById('phone').value;
+        contact._address=document.getElementById('address').value;
+        contact._city=document.getElementById('city').value;
+        contact._state=document.getElementById('state').value;
+        contact._zip=document.getElementById('zip').value;
+        console.log(contact);
+    }catch (exception) {
+        console.error(exception);
+    }
+    updateToLocalStorage();
+}
+
+function updateToLocalStorage(){
+    let record = JSON.parse(localStorage.getItem("Record"));
+    if(record){
+        let dataToUpdate = record.find(bookObj=>bookObj._id ==newContact._id);
+        
+        if(!dataToUpdate){
+            contact._id=getID();
+            record.push(contact);
+        } else {
+            console.log("rem");
+            const index = record.map(bookObj => bookObj._id)
+                                .indexOf(dataToUpdate._id);
+            contact._id=getID();
+            record.splice(index, 1, contact);
+        }
+    }else{
+        contact._id=getID();
+        record=[contact];
+    }
+
+    console.log(record);
+    localStorage.setItem("Record",JSON.stringify(record));
+    window.location.replace(site_properties.home_page);
+}
+
+function resetForm(){
+    document.getElementById("ContactForm").reset();
+    resetErr('.text-error');
+    resetErr('.phone-error');
+    resetErr('.address-error');
+}
+
+function resetErr(type){
+    document.querySelector(type).textContent="";
+}
+
+function getID(){
+    let contID = localStorage.getItem("ID");
+    contID = !contID?1:(parseInt(contID)+1).toString();
+    localStorage.setItem("ID",contID);
+    return contID;
+}
+
+function checkForUpdates(){
+    const contactJson = localStorage.getItem('editContact');
+    isUpdate = contactJson?true:false;
+    if(!isUpdate) return;
+    newContact = JSON.parse(contactJson);
+    console.log(newContact)
+    setForm();
+}
+
+function setForm(){
+    setValue('#name',newContact._name);
+    setValue('#phone',newContact._phone);
+    setValue('#address',newContact._address);
+    setValue('#city',newContact._city);
+    setValue('#state',newContact._state);
+    setValue('#zip',newContact._zip);
+}
+
+function setValue(property,value){
+    const element = document.querySelector(property);
+    element.value = value;
+}
---
+++ Scripts/Home Page.js
@@ -0,0 +1,67 @@
+let record=new Array();
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    record = getRecordFromLocalStorage();
+    createInnerHTML();
+    localStorage.removeItem("editContact");
+});
+
+function getRecordFromLocalStorage(){
+    return localStorage.getItem("Record") ?
+        JSON.parse(localStorage.getItem("Record")) : [];
+};
+
+function createInnerHTML(){
+    const headerHTML=
+        "<th>Fullname</th>"+
+        "<th>Address</th>"+
+        "<th>City</th>"+
+        "<th>State</th>"+
+        "<th>Zip Code</th>"+
+        "<th>Phone No</th>";
+
+    if (record.length==0){
+        console.log("No data found in Local Storage")
+        return;
+    }
+    let innerHTML=`${headerHTML}`;
+    for(const contact of record){
+        innerHTML=`${innerHTML}
+        <tr>
+            <td>${contact._name}</td>
+            <td>${contact._address}</td>
+            <td>${contact._city}</td>
+            <td>${contact._state}</td>
+            <td>${contact._zip}</td>
+            <td>${contact._phone}</td>
+            <td>
+                <img id="${contact._id}" src="../Assets/icons/delete-black-18dp.svg" id="${contact._id}" alt="delete" onclick="removeCont(this)"><br>
+                <img id="${contact._id}" src="../Assets/icons/create-black-18dp.svg" id="${contact._id}" alt="edit" onclick="update(this)">
+            </td>
+        </tr>
+    `;
+    }
+    document.querySelector('#table').innerHTML = innerHTML;
+}
+
+function removeCont(node){
+    let contact = record.find(bookObj=>bookObj._id == node.id);
+    if(!contact){
+        console.log("No entry found!!");
+        return;
+    }
+    const index = record.map(bookObj=>bookObj._id)
+                        .indexOf(contact._id);
+    record.splice(index,1);
+    localStorage.setItem("Record",JSON.stringify(record));
+    createInnerHTML();
+}
+function update(node){
+    let contact = record.find(bookObj=>bookObj._id == node.id);
+    if(!contact){
+        console.log("No entry found!!");
+        return;
+    }
+    localStorage.setItem('editContact', JSON.stringify(contact,'\t', 2));
+    window.location.replace(site_properties.contact_form);
+}
